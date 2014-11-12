6cba8c79dde40d54be2e9532764c45fef4275f96
Krittika DSilva <krittika.dsilva@gmail.com>
2014-11-11 20:49:02 -0800

    Testing commit

--- cgnet-test/src/cgnet/swara/activity/ActivityTest.java
+++ cgnet-test/src/cgnet/swara/activity/ActivityTest.java
@@ -3,16 +3,37 @@ package cgnet.swara.activity;
 
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import org.junit.Before;
+import org.cgnet.swara.R;
+
+import org.junit.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import static org.junit.Assert.*;
+import android.widget.Button;
+import android.widget.EditText;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+ 
+@Config(manifest = "../FeedEx-master/AndroidManifest.xml")
 @RunWith(RobolectricTestRunner.class)
 public class ActivityTest {
 	MainActivity activity;
 
     @Before
     public void setup() {
-        this.activity = Robolectric.buildActivity(MainActivity.class).create().get();
+        activity = Robolectric.buildActivity(MainActivity.class).create().get();
+ 
+        Button recordMessage =  (Button) activity.findViewById(R.id.one);
+		Button listenMessages = (Button) activity.findViewById(R.id.two);
+		EditText results =   (EditText) activity.findViewById(R.id.phone);
+		results.setText("1");
+		assertEquals("Go", "Go"); 
+    }
+      
+    
+    @Test
+    public void testTest() throws Exception {
+    	assertEquals("Go", "o"); 
     }
 }
